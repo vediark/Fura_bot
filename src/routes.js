f57ad9a0f8b4e55db1769ed474fcd90377793f8c
vediark <vediark@gmail.com>
2025-10-08 16:23:04 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,18 @@
+module.exports = function(app) {
+  // Роуты для разных страниц
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/templates/index.html");
+  });
 
+  app.get("/shop", (req, res) => {
+    res.sendFile(__dirname + "/templates/shop.html");
+  });
+
+  app.get("/stories", (req, res) => {
+    res.sendFile(__dirname + "/templates/stories.html");
+  });
+
+  app.get("/crypto", (req, res) => {
+    res.sendFile(__dirname + "/templates/crypto.html");
+  });
+};
